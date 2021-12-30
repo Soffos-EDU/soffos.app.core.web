1c1119fb967cf65fe31d6653e45941cec3cd2f81
Rafanoli <oliveira@live.in>
2021-12-30 08:07:53 -0300

    feat: add theme fonts

--- components/custom-app/DesignSystem.js
+++ components/custom-app/DesignSystem.js
@@ -1,11 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 
-const DesignSystem = ({ children }) => (
-  <ChakraProvider theme={children.props.theme} resetCSS>
-    <Global styles={children.props.fonts} />
-    {children}
-  </ChakraProvider>
-);
+const DesignSystem = ({ children }) => {
+  return (
+    <ChakraProvider theme={children.props.theme} resetCSS>
+      <Global styles={children.props.theme.fonts} />
+      {children}
+    </ChakraProvider>
+  );
+};
 
 export default DesignSystem;
